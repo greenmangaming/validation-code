09a409f15b7ad78d0b5c7b7cf622eefd30a49675
Owain Williams <owainwilliams@Ians-7plus.localdomain>
2018-07-25 16:33:14 +0100

    Validation Code
    - Simplified code and removed some files.

--- validate.py
+++ validate.py
@@ -9,9 +9,6 @@ from botocore.exceptions import ClientError
 divider = '===================================================================='
 client = boto3.client('cloudformation')
 
-def confirm(path):
-    print('Looking for files in path: {}'.format(path))
-
 def checkValidity(folder, filename):
     json = open('{}/{}'.format(folder, filename), 'r').read()
     try:
@@ -19,49 +16,39 @@ def checkValidity(folder, filename):
     except ClientError as e:
         return('\n{}\n'.format(e))
 
+def checkFiles(pathLocation):
+    errors_ = {}
+    for filename in os.listdir(pathLocation):
+        response = checkValidity(pathLocation, filename)
+        if response != None:
+            errors_[filename] = response
+    return(errors_)
+
+
 @click.command()
 @click.option('--path', prompt='path', help='The path of the CloudFormation folder.')
 
 
 def main (path):
-    num = 0
-    errors = {}
-    confirm(path)
     path_mgmnt = '{}/{}'.format(path, 'mgmnt')
-    for filename in os.listdir(path_mgmnt):
-        num += 1
-        response = checkValidity(path_mgmnt, filename)
-        if response != None:
-            key = '{}_{}'.format(filename, num)
-            errors[key] = response
-
     path_prod = '{}/{}'.format(path, 'prod')
-    for filename in os.listdir(path_prod):
-        num += 1
-        response = checkValidity(path_prod, filename)
-        if response != None:
-            key = '{}_{}'.format(filename, num)
-            errors[key] = response
-
     path_staging = '{}/{}'.format(path, 'staging')
-    for filename in os.listdir(path_staging):
-        num += 1
-        response = checkValidity(path_staging, filename)
-        if response != None:
-            key = '{}_{}'.format(filename, num)
-            errors[key] = response
+    errors = {}
+    if checkFiles(path_staging) != None:
+        errors.update(checkFiles(path_staging))
+    if checkFiles(path_prod) != None:
+        errors.update(checkFiles(path_prod))
+    if checkFiles(path_mgmnt) != None:
+        errors.update(checkFiles(path_mgmnt))
+    keys = list(errors.keys())
+    values = list(errors.values())
+    for i in range(len(keys)):
+        print(  divider + '\n' +
+                keys[i] + '\n' +
+                divider + '\n' +
+                values[i])
 
-    if len(errors.keys()) != 0:
-        # I would use 'for key, value in errors:' but there are too
-        # many values to unpack
-        for i in range(len(errors.keys())):
-            keys = list(errors.keys())
-            print('{}\n{}\n{}\n\n\n{}\n\n\n'.format(
-                            divider, keys[i], divider, errors[keys[i]]))
-        print('\n\n{}\n{}\nIn total there were {} errors.\n{}\n{}'.format(
-                        divider, divider, len(errors.keys()), divider, divider))
-    else:
-        sys.exit()
+
 
 if __name__ == '__main__':
     main()
