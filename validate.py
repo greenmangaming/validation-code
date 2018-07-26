d2ccdaf8720e52229c36a08f9458bc6f4f83f012
Owain Williams <owainwilliams@Ians-7plus.localdomain>
2018-07-26 16:50:47 +0100

    Validation Code
    - Fixed wrong document.

--- validate.py
+++ validate.py
@@ -3,6 +3,8 @@ import os
 import click
 import sys
 from botocore.exceptions import ClientError
+import threading
+
 
 divider = '===================================================================='
 client = boto3.client('cloudformation')
@@ -17,13 +19,31 @@ def checkValidity(folder, filename):
 def checkFiles(pathLocation):
     errors_ = {}
     for filename in os.listdir(pathLocation):
-        response = checkValidity(pathLocation, filename)
-        if response != None:
-            errors_[filename] = response
+        if filename.endswith('.json'):
+            response = checkValidity(pathLocation, filename)
+            if response != None:
+                errors_[filename] = response
     return(errors_)
 
+def upload(pathLocation, filename, s3Dir, s3Bucket):
+    if not filename.endswith('.json'):
+        return
+    s3Bucket.put_object(Key='gmg-interns/owainwi/{}/{}'.format(s3Dir, filename),
+                    Body=open('{}/{}'.format(pathLocation, filename), 'r').read())
+    print('{} uploaded to s3!'.format(filename))
+
+def uploadFiles(pathLocation, s3Dir):
+    s3 = boto3.resource('s3')
+    bucket = 'gmg-general-dev-test'
+    s3Bucket = s3.Bucket(bucket)
+    filenames = []
+    for filename in os.listdir(pathLocation):
+        filenames.append(filename)
+    for fname in filenames:
+        t = threading.Thread(target = upload, args = (pathLocation, fname, s3Dir, s3Bucket)).start()
+
 @click.command()
-@click.option('--path', prompt='path', help='The path of the CloudFormation folder.')
+@click.option('--path', prompt='Path', help='The Path of the CloudFormation folder.')
 
 def main(path):
     path_mgmnt = '{}/{}'.format(path, 'mgmnt')
@@ -43,7 +63,14 @@ def main(path):
                 keys[i] + '\n' +
                 divider + '\n' +
                 values[i])
-    sys.exit()
+    if len(keys) == 0:
+        uploadFiles(path_mgmnt, 'mgmnt')
+        uploadFiles(path_prod, 'prod')
+        uploadFiles(path_staging, 'staging')
+        sys.exit()
+    else:
+        print('There were errors so the files could not be uploaded to s3.')
+        sys.exit()
 
 if __name__ == '__main__':
     main()
