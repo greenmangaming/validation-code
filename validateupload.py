da022582470857ed3843b3ca277ada0163c4eda1
Owain Williams <owainwilliams@Ians-7plus.localdomain>
2018-07-26 15:15:04 +0100

    Validation Code
    - Code now uploads to s3 with threading, takes roughly 12 seconds.

--- validateupload.py
+++ validateupload.py
@@ -3,6 +3,8 @@ import os
 import click
 import sys
 from botocore.exceptions import ClientError
+import threading
+
 
 divider = '===================================================================='
 client = boto3.client('cloudformation')
@@ -17,23 +19,31 @@ def checkValidity(folder, filename):
 def checkFiles(pathLocation):
     errors_ = {}
     for filename in os.listdir(pathLocation):
-        response = checkValidity(pathLocation, filename)
-        elif response != None:
-            errors_[filename] = response
-        elif (os.stat('.gitignore').st_size > 51200):
-            sys.exit('File size too large (>51200 B)')
+        if filename.endswith('.json'):
+            response = checkValidity(pathLocation, filename)
+            if response != None:
+                errors_[filename] = response
     return(errors_)
 
-def uploadFiles(pathLocation):
+def upload(pathLocation, filename, s3Dir, s3Bucket):
+    if not filename.endswith('.json'):
+        return
+    s3Bucket.put_object(Key='gmg-interns/owainwi/{}/{}'.format(s3Dir, filename),
+                    Body=open('{}/{}'.format(pathLocation, filename), 'r').read())
+    print('{} uploaded to s3!'.format(filename))
+
+def uploadFiles(pathLocation, s3Dir):
+    s3 = boto3.resource('s3')
+    bucket = 'gmg-general-dev-test'
+    s3Bucket = s3.Bucket(bucket)
+    filenames = []
     for filename in os.listdir(pathLocation):
-        s3 = boto3.resource('s3')
-        s3Bucket = s3.Bucket('s3://gmg-general-dev-test/gmg-interns/owainwi/')
-        s3Bucket = .put_object(Key=filename,
-                    Body=open('{}/{}'.format(folder, filename), 'r').read())
-        print('{} uploaded to s3!'.format(filename))
+        filenames.append(filename)
+    for fname in filenames:
+        t = threading.Thread(target = upload, args = (pathLocation, fname, s3Dir, s3Bucket)).start()
 
 @click.command()
-@click.option('--Path', prompt='Path', help='The Path of the CloudFormation folder.')
+@click.option('--path', prompt='Path', help='The Path of the CloudFormation folder.')
 
 def main(path):
     path_mgmnt = '{}/{}'.format(path, 'mgmnt')
@@ -54,9 +64,9 @@ def main(path):
                 divider + '\n' +
                 values[i])
     if len(keys) == 0:
-        uploadFiles(path_mgmnt)
-        uploadFiles(path_prod)
-        uploadFiles(path_staging)
+        uploadFiles(path_mgmnt, 'mgmnt')
+        uploadFiles(path_prod, 'prod')
+        uploadFiles(path_staging, 'staging')
         sys.exit()
     else:
         print('There were errors so the files could not be uploaded to s3.')
