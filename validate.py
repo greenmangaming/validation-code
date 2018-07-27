4ade8b1a123f6a0bff0f7388e1066ef7b8c59580
Owain Williams <owainwilliams@Ians-7plus.localdomain>
2018-07-27 15:54:38 +0100

    Validation Code
    - Validation now uploads the files to s3 before
      validating, increasing the file-size limit.

--- validate.py
+++ validate.py
@@ -4,50 +4,38 @@ import click
 import sys
 from botocore.exceptions import ClientError
 import threading
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 
+# /Users/owainwilliams/Desktop/validation-code/cloudformation/
+# gmg-general-dev-test/gmg-interns/owainwi
 
 divider = '=' * 70
 client = boto3.client('cloudformation')
 
-def checkValidity(root, filename):
-    json = open('{}/{}'.format(root, filename), 'r').read()
+def checkValidity(bucket, val):
+    url = 'https://s3.amazonaws.com/' + bucket + '/' + val
     try:
-        response = client.validate_template(TemplateBody=json)
+        response = client.validate_template(TemplateURL=url)
     except ClientError as e:
-        return('\n{}\n'.format(e))
+        print('error - ' + url)
+        return(e)
 
-def checkFiles(path):
-    errors_ = {}
-    paths = []
-    for root, dirs, files in os.walk(path):
-        paths.append(root)
-        for filename in os.listdir(root):
-            if not filename.endswith('.json'):
-                continue
-            response = checkValidity(root, filename)
-            if response != None:
-                errors_[filename] = response
-    return(errors_, paths)
+def checkFiles(bucket, keys):
+    errors = []
+    for val in keys:
+        response = checkValidity(bucket, val)
+        if response == None:
+            continue
+        errors.append({
+            'file': val,
+            'error': response
+        })
+    return(errors)
 
-def upload(path, filename, s3Dir, s3Bucket, subDir):
-    if not filename.endswith('.json'):
-        return
-    s3Bucket.put_object(
-        Key='{}{}/{}'.format(s3Dir, subDir, filename),
-        Body=open('{}/{}'.format(path, filename), 'r'
-    ).read())
-    print('{} uploaded to s3!'.format(filename))
-
-def uploadFiles(path, s3Dir, orDir):
-    s3 = boto3.resource('s3')
-    bucket = 'gmg-general-dev-test'
-    s3Bucket = s3.Bucket(bucket)
-    subDir = path[len(orDir):]
-    for filename in os.listdir(path):
-        t = threading.Thread(
-            target = upload,
-            args = (path, filename, s3Dir, s3Bucket, subDir)
-        ).start()
+def upload(body, key, s3Bucket):
+    s3Bucket.put_object(Key = key, Body = body)
+    return(key + ' has been uploaded to s3')
 
 @click.command()
 @click.option(
@@ -62,34 +50,48 @@ def uploadFiles(path, s3Dir, orDir):
 )
 
 def main(path, dir):
-    errors = {}
-    checkFilesRes = checkFiles(path)
 
-    if dir.endswith('/'):
-        dir = dir[:-1]
+    keys = []
+    fs = []
+    paths = []
+    s3 = boto3.resource('s3')
+    bucket = 'gmg-general-dev-test'
+    s3Bucket = s3.Bucket(bucket)
+    if not dir.endswith('/'):
+        dir = dir + '/'
     if dir.startswith('/'):
         dir = dir[1:]
-    errors.update(checkFilesRes[0])
 
-    keys = list(errors.keys())
-    values = list(errors.values())
+    for root, dirs, files in os.walk(path):
+        paths.append(root)
 
-    for i in range(len(keys)):
-        print(  divider + '\n' +
-                keys[i] + '\n' +
-                divider + '\n' +
-                values[i])
-
-    if len(keys) == 0:
-
-        paths = checkFilesRes[1]
-
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as pool:
         for val in paths:
-            uploadFiles(val, dir, paths[0])
-        sys.exit()
+            sub = val[len(paths[0]):]
+            for filename in os.listdir(val):
+                if not filename.endswith('.json'):
+                    continue
+                key = dir + sub + '/' + filename
+                body = open(val + '/' + filename, 'r').read()
+                keys.append(key)
+                future = pool.submit(upload, body, key, s3Bucket)
+                fs.append(future)
+        for future in concurrent.futures.as_completed(fs):
+            print(future.result())
+
+    s3Location = bucket + '/' + dir
+    errors = checkFiles(bucket, keys)
+
+    if len(errors) > 0:
+        for i in range(len(errors)):
+            print(
+                divider + '\n' +
+                errors[i]['file'] + '\n' +
+                divider + '\n'
+            )
+            print(errors[i]['error'])
     else:
-        print('There were errors so the files could not be uploaded to s3.')
-        sys.exit()
+        print('There were no errors!')
 
 if __name__ == '__main__':
     main()
